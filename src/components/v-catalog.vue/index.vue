e8db68cb83162bf0b1dcab4979f2432ca56dae27
Alex <alexmlrdv@gmail.com>
2021-07-12 22:45:27 +0200

    LESSON 3

---
+++ src/components/v-catalog.vue/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="v-catalog.vue">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'V-catalog.vue',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
---
+++ src/components/v-catalog/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="v-catalog">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'V-catalog',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
